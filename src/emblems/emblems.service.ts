783f82b85e38fd9c16437e19bce476d5831a79f4
misasoares <misasoaresms2@gmail.com>
2024-06-16 23:57:18 -0300

    doc: remove unused methods

--- src/emblems/emblems.service.ts
+++ src/emblems/emblems.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEmblemDto } from './dto/create-emblem.dto';
-import { UpdateEmblemDto } from './dto/update-emblem.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EmblemsService {
-  create(createEmblemDto: CreateEmblemDto) {
-    return 'This action adds a new emblem';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all emblems`;
+  async findAll() {
+    const emblems = await this.prisma.emblems.findMany();
+    return emblems;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} emblem`;
-  }
-
-  update(id: number, updateEmblemDto: UpdateEmblemDto) {
-    return `This action updates a #${id} emblem`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} emblem`;
   }
 }
