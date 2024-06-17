23b9981b50e9886a5d559181c471c10566269cac
misasoares <misasoaresms2@gmail.com>
2024-06-16 23:58:33 -0300

    feature: add api property to see on swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,18 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
-import { User } from '../entities/user.entity';
 
-export class CreateUserDto extends User {
+export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(4)
   @MaxLength(20)
   password: string;
 
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   nickname: string;
 }
